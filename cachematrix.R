e273432a81b6a15a7a000876618b88e186418e35
omarmar3y <omer4162@gmail.com>
2020-10-05 13:14:11 +0200

    i made two functions one that take matrix and create structure that can save the cache of it's inverse it returns a list of functions that can set and get the cache and the matrix and second one check if  the given matrix has cache if it has one it returns it if not it inverse it and cache the result  and return it

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y){
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() x
+  
+  setcache <- function(c) cache <<- c
+  getcache <- function() cache
+  list(set = set, get = get , getcache = getcache,setcache = setcache)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <- x$getcache()
+  if(!is.na(cache)){
+    print('get cached matrix')
+    return(cache)
+  }
+  data <- solve(x$get())
+  x$setcache(data)
+  data
+  
+  ## Return a matrix that is the inverse of 'x'
 }
